5a8c2c8caa198d42e683e5df869c5f65fef3765e
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2024-12-28 16:35:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ if st.session_state.choices and not st.session_state.mmid:
         st.session_state.match_selected = True
     # Store selected match ID in session state
         st.session_state.mmid = match_id
-if st.session_state.mmid:
+if st.session_state.mmid and not st.session_state.toa:
     types_of_analysis = ["head to head", "playerwise", "future prediction", "numerology"]
     analysis_choice = st.selectbox("Analysis Type", types_of_analysis)
     
