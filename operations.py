ec5222a63378348f635c0463cd060db84a4ebc7e
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2024-12-30 16:01:05 +0530

    Update operations.py

--- operations.py
+++ operations.py
@@ -136,7 +136,7 @@ def match_id_init():
 
     headers = {
             'sec-ch-ua-platform': '"Windows"',
-            'Referer': 'https://www.fotmob.com/players/1083323/pedri',
+            'Referer': 'https://www.fotmob.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0',
             'x-mas': 'eyJib2R5Ijp7InVybCI6Ii9hcGkvcGxheWVyU3RhdHM/cGxheWVySWQ9MTA4MzMyMyZzZWFzb25JZD0wLTEmaXNGaXJzdFNlYXNvbj1mYWxzZSIsImNvZGUiOjE3MzU0NDg2MjI1NDIsImZvbyI6ImU5NmI2MGEyMSJ9LCJzaWduYXR1cmUiOiIzM0FENzdFNzI5NTk2OTVBRDFGRERFM0JEQjMxQkFFOCJ9',
             'sec-ch-ua': '"Microsoft Edge";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
