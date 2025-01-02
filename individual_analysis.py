0a378f06d50e2da2ecad1807f66d3189aacf93bb
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-03 00:01:49 +0530

    Update individual_analysis.py

--- individual_analysis.py
+++ individual_analysis.py
@@ -340,6 +340,17 @@ def main():
             st.session_state.per90=False
         if "indivs" not in st.session_state:
             st.session_state.indivs=False
+        pos=st.selectbox("Choose position to analyse:",list(desired.keys()))
+        if st.button("Position"):
+            st.session_state.pposition=pos
+            st.write(f"Selected position:{st.session_state.pposition}")
+        if st.session_state.pposition:
+            p9=st.selectbox("Per90?",[True,False])
+            if st.button("p90"):
+                   st.session_state.per90=p9
+                   st.write(f"Per90:{st.session_state.per90}")
+                   st.session_state.indivs=True
+            st.warning("please click 'Finished1' if u want to change options")
         #st.write(st.session_state)
         #get_data_destruct()
       
@@ -348,17 +359,6 @@ def main():
             st.write(a)
         #st.write(st.session_state)
         if st.session_state.returned:
-            pos=st.selectbox("Choose position to analyse:",list(desired.keys()))
-            if st.button("Position"):
-                st.session_state.pposition=pos
-                st.write(f"Selected position:{st.session_state.pposition}")
-            if st.session_state.pposition:
-                p9=st.selectbox("Per90?",[True,False])
-                if st.button("p90"):
-                        st.session_state.per90=p9
-                        st.write(f"Per90:{st.session_state.per90}")
-                        st.session_state.indivs=True
-            st.warning("please click 'Finished1' if u want to change options")
             if st.session_state.indivs:
                 get_season_stats(list(st.session_state.returned.keys())[0],list(st.session_state.returned.values())[0],st.session_state.opt2)
             #get_data_destruct()
