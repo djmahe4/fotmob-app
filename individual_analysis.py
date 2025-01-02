48395337bc91c2b151ec0e8b808e1c8824f20165
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-02 23:07:36 +0530

    Update individual_analysis.py

--- individual_analysis.py
+++ individual_analysis.py
@@ -350,16 +350,16 @@ def main():
             if st.button("Position"):
                 if pos!=st.session_state.pposition:
                         st.session_state.pposition=pos
-            else:
-                st.session_state.pposition=pos
+                else:
+                        st.session_state.pposition=pos
             st.write(f"Selected position:{st.session_state.pposition}")
             if st.session_state.pposition:
                 p9=st.selectbox("Per90?",[True,False])
                 if st.button("p90"):
                         if p9 != st.session_state.per90:
                                 st.session_state.per90=p9
-                else:
-                        st.session_state.per90=p9
+                        else:
+                                st.session_state.per90=p9
                 st.write(f"Per90:{st.session_state.per90}")
                 st.session_state.indivs=True
             st.warning("please click 'Finished1' if u want to change options")
