0ceb31ee5cf145a0f86bd3da685c7f1e999f7d6e
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-02 16:56:39 +0530

    Update check.py

--- check.py
+++ check.py
@@ -174,8 +174,8 @@ def get_season_stats(name,id=1083323,season="LaLiga"):
     }
     pos=st.selectbox("Choose position to analyse:",list(desired.keys()))
     #p9=st.selectbox("Per90?",[True,False])
-    if st.button("Selected.."):
-        st.session_state.pposition=pos
+    #if st.button("Selected.."):
+    st.session_state.pposition=pos
         #st.session_state.per90=p9
     present=[]
     necessary = []
@@ -193,10 +193,10 @@ def get_season_stats(name,id=1083323,season="LaLiga"):
     print(present)
     #if p9!=st.session_state.per90:
         #st.session_state.per90=p9
-    p9 = input("Per 90 stats (y/n):")
+    #p9 = input("Per 90 stats (y/n):")
     p9=st.selectbox("Per90?",[True,False])
-    if st.button("Done.."):
-        st.session_state.per90=p9
+    #if st.button("Done.."):
+    st.session_state.per90=p9
     # Convert the data into a pandas DataFrame
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
