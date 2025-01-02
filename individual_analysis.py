3607c81fd9a6a662cd653502f6bc5aca8dbbf8ce
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-02 23:04:03 +0530

    Update individual_analysis.py

--- individual_analysis.py
+++ individual_analysis.py
@@ -362,8 +362,8 @@ def main():
                                 st.session_state.per90=p9
                         st.write(f"Per90:{st.session_state.per90}")
                         st.session_state.indivs=True
-           st.warning("please click 'Finished1' if u want to change options")
-           if st.session_state.indivs:
+            st.warning("please click 'Finished1' if u want to change options")
+            if st.session_state.indivs:
                 get_season_stats(list(st.session_state.returned.keys())[0],list(st.session_state.returned.values())[0],st.session_state.opt2)
             #get_data_destruct()
         if st.button("Finished1"):
