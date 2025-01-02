f027d13be516bc9a13ccc4a3e1f7821cf3ea16f3
Maheshwar Anup <137691824+djmahe4@users.noreply.github.com>
2025-01-02 18:53:19 +0530

    Update check.py

--- check.py
+++ check.py
@@ -245,7 +245,7 @@ def get_season_stats(name,id=1083323,season="LaLiga"):
     st.session_state.pposition=pos
     p9=st.selectbox("Per90?",[True,False])
     st.session_state.per90=p9
-    st.button("Selected..",on_click=new_plot_funct(b)):
+    st.button("Selected..",on_click=new_plot_funct(b))
         #new_plot_funct(b)
         #st.session_state.pposition=pos
         #st.session_state.per90=p9
